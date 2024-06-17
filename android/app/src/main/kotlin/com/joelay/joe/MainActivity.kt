07865dc4f686f6dbff99da4161b273e62f193b8b
Administrator <webmaster.tsuyoshi@gmail.com>
2024-06-17 12:46:21 -0700

    seeu

---
+++ android/app/src/main/kotlin/com/joelay/joe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.joelay.joe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'screens/home_page.dart';
+
+void main() {
+
+  // Initialize webview_flutter for Android
+
+
+  runApp(MangaReaderApp());
+}
+
+class MangaReaderApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Manga Reader',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
